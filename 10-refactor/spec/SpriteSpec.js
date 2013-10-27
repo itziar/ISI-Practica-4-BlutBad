9db7d23e8d152c45766e326d77db08e4c0b8a3be
Serhii Slipenko <LeSagra@gmail.com>
2013-10-28 00:09:05 +0100

    it del setup, si se crean campos en el objeto

--- 10-refactor/spec/SpriteSpec.js
+++ 10-refactor/spec/SpriteSpec.js
@@ -1,43 +1,42 @@
 describe("10 - SpriteSpec", function() {
-	  beforeEach(function(){
-var sprites = {
-                    ship: {
-                        sx: 0,
-                        sy: 0,
-                        w: 37,
-                        h: 42,
-                        frames: 1
-                    }
-                }
+    beforeEach(function() {
+        var sprites = {
+            ship: {
+                sx: 0,
+                sy: 0,
+                w: 37,
+                h: 42,
+                frames: 1
+            }
+        }
 
         SpriteSheet.map = sprites;
-       });
+    });
 
-        it("sprite defined & set", function() {
+    it("sprite defined & set", function() {
 
-            var spro = function() {
-            	this.setup("ship", {
-            	});
-            }
-            spro.prototype = new Sprite()
-            sp = new spro()
-            expect(SpriteSheet.map[sp.sprite]).toBeDefined();
-            expect(sp.sprite).toEqual('ship');
-        });
+        var spro = function() {
+            this.setup("ship", {});
+        }
+        spro.prototype = new Sprite()
+        sp = new spro()
+        expect(SpriteSheet.map[sp.sprite]).toBeDefined();
+        expect(sp.sprite).toEqual('ship');
+    });
 
-        it("setup", function() {
-                
-            var spro = function() {
-            	this.setup("ship", {
-            		var1 :1,
-            		var2 :2
-            	});
-            }
-            spro.prototype = new Sprite()
-            sp = new spro()
-            expect(sp.var1).toBeDefined();
-            expect(sp.var2).toBeDefined();
-            expect(sp.var1).toBe(1);
-            expect(sp.var2).toBe(2);
-        });
+    it("setup", function() {
+
+        var spro = function() {
+            this.setup("ship", {
+                var1: 1,
+                var2: 2
+            });
+        }
+        spro.prototype = new Sprite()
+        sp = new spro()
+        expect(sp.var1).toBeDefined();
+        expect(sp.var2).toBeDefined();
+        expect(sp.var1).toBe(1);
+        expect(sp.var2).toBe(2);
+    });
 });
