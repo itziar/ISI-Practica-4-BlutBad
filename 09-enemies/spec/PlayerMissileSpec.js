19149cf4cc441041527410b7cc77e77ac740da63
Serhii Slipenko <LeSagra@gmail.com>
2013-10-27 14:34:18 +0100

    formato del archivo

--- 09-enemies/spec/PlayerMissileSpec.js
+++ 09-enemies/spec/PlayerMissileSpec.js
@@ -2,40 +2,42 @@
 
   Requisitos: 
 
-  La nave del usuario disparar· 2 misiles si est· pulsada la tecla de
+  La nave del usuario disparar√° 2 misiles si est√° pulsada la tecla de
   espacio y ha pasado el tiempo de recarga del arma.
 
-  El arma tendr· un tiempo de recarga de 0,25s, no pudiÈndose enviar
+  El arma tendr√° un tiempo de recarga de 0,25s, no pudi√©ndose enviar
   dos nuevos misiles antes de que pasen 0,25s desde que se enviaron
   los anteriores
 
 
 
-  EspecificaciÛn:
+  Especificaci√≥n:
 
-  - Hay que aÒadir a la variable sprites la especificaciÛn del sprite
+  - Hay que a≈Ñadir a la variable sprites la especificaci√≥n del sprite
     missile
 
-  - Cada vez que el usuario presione la tecla de espacio se aÒadir·n
-    misiles al tablero de juego en la posiciÛn en la que estÈ la nave
-    del usuario. En el cÛdigo de la clase PlayerSip es donde tienen
-    que aÒadirse los misiles
+  - Cada vez que el usuario presione la tecla de espacio se a≈Ñadir√°n
+    misiles al tablero de juego en la posici√≥n en la que est√© la nave
+    del usuario. En el c√≥digo de la clase PlayerSip es donde tienen
+    que a≈Ñadirse los misiles
 
   - La clase PlayerMissile es la que implementa los misiles. Es
-    importante que la creaciÛn de los misiles sea poco costosa pues va
-    a haber muchos disparos, para lo cual se declarar·n los mÈtodos de
+    importante que la creaci√≥n de los misiles sea poco costosa pues va
+    a haber muchos disparos, para lo cual se declarar√°n los m√©todos de
     la clase en el prototipo
 
 */
-describe("Clase PlayerMissile", function(){
+hola
+
+describe("Clase PlayerMissile", function() {
 
 
-    it("defined missile in spriteSheet", function(){
+    it("defined missile in spriteSheet", function() {
         SpriteSheet.map['missile'] = sprites.missile;
         expect(SpriteSheet.map['missile']).toBeDefined();
     });
-
-    it("add 2 missile to spriteSheet", function(){
+    Gam
+    it("add 2 missile to spriteSheet", function() {
         board = new GameBoard();
         pShip = new PlayerShip();
         pShip.reload = -1;
@@ -46,6 +48,7 @@ describe("Clase PlayerMissile", function(){
         board.step(20);
         Game.keys['fire'] = false;
         board.step(20);
+
         Game.keys['fire'] = true;
         board.step(20);
         expect(board.objects.length).toBe(3);
@@ -53,22 +56,21 @@ describe("Clase PlayerMissile", function(){
     });
 
 
-    it("draw missile",function(){
+    it("draw missile", function() {
         SpriteSheet.map['missile'] = sprites.missile;
         board = new GameBoard();
         //pShip = new PlayerShip();
-        ctx={};
+        ctx = {};
         var m1 = new PlayerMissile();
         var m2 = new PlayerMissile();
 
-        spyOn(m1,'draw');
-        spyOn(m2,'draw');
-        board.add(m1); 
-        board.add(m2); 
+        spyOn(m1, 'draw');
+        spyOn(m2, 'draw');
+        board.add(m1);
+        board.add(m2);
         board.draw(ctx)
         expect(m1.draw).toHaveBeenCalled();
         expect(m2.draw).toHaveBeenCalled();
     });
 
 });
-
